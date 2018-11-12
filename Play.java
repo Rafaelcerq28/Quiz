69c350af620d4a7f4179281f6a38a3612c54646e
Rafaelcerq28 <33039744+Rafaelcerq28@users.noreply.github.com>
2018-11-12 00:20:06 -0200

    Create Play.java
    Classe Principal

---
+++ Play.java
@@ -0,0 +1,22 @@
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+public class Play {
+
+	public static void main(String[] args) throws SQLException {
+
+		
+		
+		Jogo.menu();
+				
+	}
+
+	
+
+}
