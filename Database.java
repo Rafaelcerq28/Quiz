51b93f543b7f57b6ee7f81569a99d3616fcda2a7
Rafaelcerq28 <33039744+Rafaelcerq28@users.noreply.github.com>
2018-11-13 22:05:39 -0200

    Update Database.java

--- Database.java
+++ Database.java
@@ -12,7 +12,7 @@ public class Database {
 	
 	public static Connection conexao() throws SQLException {
 		try{
-			Connection c = DriverManager.getConnection("jdbc:mysql://localhost/teste","root","");
+			Connection c = DriverManager.getConnection("jdbc:mysql://localhost/quiz","root","");
 		
 			return c;
 		}catch(SQLException ex) {
